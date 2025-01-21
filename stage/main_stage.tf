e1d3e5d1149169b98fd557ab197af86294d69417
jaydangar <jayjaydangar96@gmail.com>
2025-01-21 02:29:44 -0500

    Added route tables and igw for the public subnets

--- stage/main_stage.tf
+++ stage/main_stage.tf
@@ -20,3 +20,16 @@ module "subnets" {
   source = "../modules/subnets"
   vpc_id = module.vpc.vpc_id
 }
+
+module "route_table" {
+  for_each = module.subnets.public_subnets
+  source = "../modules/route_table"
+  vpc_id = module.vpc.vpc_id
+  internet_gateway_id = module.igw.igw_id
+  subnet_id = each.value
+}
+
+module "igw" {
+  source = "../modules/igw"
+  vpc_id = module.vpc.vpc_id
+}
