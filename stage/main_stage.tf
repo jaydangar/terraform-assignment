9efb09814081c6a811dea7b398e47e6a130ce4b5
jaydangar <jayjaydangar96@gmail.com>
2025-01-21 01:59:38 -0500

    returning a list of subnets instead of individual

--- stage/main_stage.tf
+++ stage/main_stage.tf
@@ -3,20 +3,16 @@ provider "aws" {
 }
 
 module "private_ec2_instances" {
-  source          = "../modules/ec2/"
-  for_each        = {
-    subnet1 = { subnet_id = module.subnets.private_subnet1 }
-    subnet2 = { subnet_id = module.subnets.private_subnet2 }
-    subnet3 = { subnet_id = module.subnets.private_subnet3 }
-  }
-  is_prod         = false
-  user_data_path  = "../modules/ec2/bash.sh"
-  subnet_id       = each.value.subnet_id
-  vpc_id = module.vpc.vpc_id
+  source         = "../modules/ec2/"
+  for_each       = module.subnets.public_subnets
+  is_prod        = false
+  user_data_path = "../modules/ec2/bash.sh"
+  subnet_id      = each.value
+  vpc_id         = module.vpc.vpc_id
 }
 
 module "vpc" {
-  source = "../modules/vpc"
+  source     = "../modules/vpc"
   cidr_block = "10.0.0.0/16"
 }
 
